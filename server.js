b227b706361c8c3c925447e6d852f50c750e70e1
Isabelle <isabellelidberg@live.se>
2018-05-28 13:45:54 +0200

    still fix error

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require('path');
 app.use(express.static('./dist/starwars-angular'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/starwars-angular/'));
+  res.sendFile("index.html", { root: path.join(__dirname, 'public') })
 });
 app.listen(process.env.PORT || 8080);
