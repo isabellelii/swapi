99f30b13fa8fcaa6c8aa5f9f2cbd7a63f835156e
Isabelle <isabellelidberg@live.se>
2018-05-30 11:22:13 +0200

    deploy changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static('./dist/starwars-angular'));
-app.get('/*/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile("index.html", { root: path.join(__dirname, 'public') })
 });
 app.listen(process.env.PORT || 8080);
