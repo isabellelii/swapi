2b2655896832a6c1792946b85cd216a54d94c9a4
Isabelle <isabellelidberg@live.se>
2018-05-30 12:50:02 +0200

    deploy error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/starwars-angular/'));
 app.get('/*', function(req, res) {
   res.sendFile("index.html", { root: path.join(__dirname, 'public') })
 });
